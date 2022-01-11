f4961f2b1a4ec2a9d761720bca5a6dad470bfedc
thomasbruchet <86605223+thomasbruchet@users.noreply.github.com>
2022-01-11 23:11:48 +0100

    Update bashrc2.sh

--- bashrc2.sh
+++ bashrc2.sh
@@ -1,13 +1,13 @@
 echo -e "\e[96m Modification du .bashrce[0m"  
 cp /home/tbr/.bashrc /home/tbr/.bashrc.bak  
-sed -i -e "s/#export/ export/" /home/tbr/.bashrc  
-sed -i -e "s/#eval/ eval/" /home/tbr/.bashrc  
-sed -i -e "s/#alias ls/ alias ls/" /home/tbr/.bashrc  
-sed -i -e "s/#alias ll/ alias ll/" /home/tbr/.bashrc  
-sed -i -e "s/#alias l/ alias l/" /home/tbr/.bashrc  
-sed -i -e "s/#alias rm/ alias rm/" /home/tbr/.bashrc  
-sed -i -e "s/PS1='/PS1='\\\n/g" /home/tbr/.bashrc 
-sed -i -e "s/#force_color_prompt/ force_color_prompt/" /home/tbr/.bashrc
+sed -i -e "s/#export/ export/" .bashrc  
+sed -i -e "s/#eval/ eval/" .bashrc  
+sed -i -e "s/#alias ls/ alias ls/" .bashrc  
+sed -i -e "s/#alias ll/ alias ll/" .bashrc  
+sed -i -e "s/#alias l/ alias l/" .bashrc  
+sed -i -e "s/#alias rm/ alias rm/" .bashrc  
+sed -i -e "s/PS1='/PS1='\\\n/g" .bashrc  
+sed -i -e "s/#force_color_prompt/ force_color_prompt/" .bashrc  
 source .bashrc  
 echo -e "\e[96m Done"  
 echo -e "  "  
