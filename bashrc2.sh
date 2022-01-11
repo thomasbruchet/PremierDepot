539a97bd73a8825646c04de79a11f79503a5e7f8
thomasbruchet <86605223+thomasbruchet@users.noreply.github.com>
2022-01-11 23:32:53 +0100

    Update bashrc2.sh

--- bashrc2.sh
+++ bashrc2.sh
@@ -1,5 +1,5 @@
-echo -e "\e[96m Modification du .bashrce[0m"  
-cp /home/tbr/.bashrc /home/tbr/.bashrc.bak  
+echo -e "\e[96m Modification du .bashrc \e[0m"  
+cp .bashrc .bashrc.bak  
 sed -i -e "s/#export/ export/" .bashrc  
 sed -i -e "s/#eval/ eval/" .bashrc  
 sed -i -e "s/#alias ls/ alias ls/" .bashrc  
@@ -8,4 +8,5 @@ sed -i -e "s/#alias l/ alias l/" .bashrc
 sed -i -e "s/#alias rm/ alias rm/" .bashrc  
 sed -i -e "s/PS1='/PS1='\\\n/g" .bashrc  
 sed -i -e "s/#force_color_prompt/ force_color_prompt/" .bashrc  
+source .bashrc
 echo -e "\e[96m Done" 
